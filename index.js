2b9d526f37b1980e0b26752502c188e33a245daf
Coşkun Atak <coskntkk@gmail.com>
2024-06-05 15:50:52 +0300

    yeni vaka

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const last = new Date(2024, 4, 28)
-const source = "https://www.iha.com.tr/samsun-haberleri/okul-muduru-heimlich-manevrasiyla-ogrencinin-hayatini-kurtardi-o-anlar-kamerada-86451179"
+const last = new Date(2024, 5, 4)
+const source = "https://www.trthaber.com/videolar/samsunda-bogazina-yemek-kacan-kiz-heimlich-manevrasiyla-kurtarildi-72863.html"
 
 const lastData = document.getElementById("last")
 lastData.innerText = `${last.getDate()}/${last.getMonth() + 1}/${last.getFullYear()}`
